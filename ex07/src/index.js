1df6d457f15759795e427992144daf67e01f09ae
hero-BA <suad.hero@gmail.com>
2021-03-19 04:06:01 +0100

    Added exercise: Return the largest number

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -19,10 +19,10 @@ function largestNumFromArr(arr) {
 
 console.log(
   largestNumFromArr([
-    [17, 23, 25, 12],
-    [25, 7, 34, 48],
-    [4, 10, 18, 21],
-    [72, 3, 17, 10],
+    [4, 9, 1, 3],
+    [13, 35, 18, 26],
+    [32, 35, 97, 39],
+    [1000000, 1001, 857, 1],
   ])
 ); // Change this line
 module.exports = largestNumFromArr;
