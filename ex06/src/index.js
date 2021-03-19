be69f88929b02b534e710e50f456e0daaeb6ccac
hero-BA <suad.hero@gmail.com>
2021-03-19 10:45:19 +0100

    Added unfinished exercise: nesting for loops

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function multiplyArrayFunction(myArray) {
+  var myArray = [...myArray];
+  let sum = 0;
+  let product = 1;
+  
+  
+  return [sum, product];
+}
+// Only change code above this line
+console.log(multiplyArrayFunction([2], [5, 6, 7], [8, 9])); // Change this line
+module.exports = multiplyArrayFunction;
