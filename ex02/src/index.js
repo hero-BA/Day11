b3b92257427f0ea5e1826787d4124483a503e855
hero-BA <suad.hero@gmail.com>
2021-03-19 10:49:29 +0100

    Added unfinished exercise: Multi-dimensional arrays

---
+++ ex02/src/index.js
@@ -0,0 +1,28 @@
+// Create a temps array here
+var temps = [
+  [31, 32, 19, 37],
+  [29, 27, 55, 36],
+  [17, 27, 42, 46],
+  [29, 52, 21, 64],
+  [91, 27, 31, 61],
+];
+// End of temps array
+function myArrayFunction(arr) {
+  var newTemps = [...arr];
+  var averageDayTemp = [];
+  // Only change below this line
+  var sum = 0;
+   for (var i = 0; i < newTemps.length; i++) {
+       for (var j = 0; j < newTemps[i].length; j++) {
+           sum += newTemps[i][j]
+       }
+   }
+   console.log(sum);
+   return averageDayTemp;
+   // Only change code above this line
+    //return averageDayTemp;
+ 
+}
+
+console.log(myArrayFunction(temps)); // Change this line
+module.exports = myArrayFunction;
