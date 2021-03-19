e5fa9fc25465a232b3a8cf8c887443f122520f01
hero-BA <suad.hero@gmail.com>
2021-03-19 10:47:13 +0100

    Added unfinished exercise: Store multiple values in one variable

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+// Create a monitorsListArray array here
+var monitorsListArray = ["Apple", "Peach", "Berry"];
+// End of monitorsListArray array
+
+function myMonitorsFunction(arr) {
+    var newMonitorsList = [...arr];
+    // Only change code below this line
+    var monitorsList = [];
+    
+
+    // Only change code above this line
+}
+
+console.log(myMonitorsFunction());  // Change this line
+module.exports = myMonitorsFunction;
