045c548c9626b7a87f9cfc7c432d227ad2ca71e0
hero-BA <suad.hero@gmail.com>
2021-03-19 03:07:40 +0100

    Added exercise: Falsy ejector

---
+++ ex09/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function myBouncer(arr) {
+  return arr.filter(Boolean);
+}
+// Only change code above this line
+
+console.log(myBouncer([null, NaN, 1, 2, undefined])); // Change this line
+module.exports = myBouncer;
