6259ae0330dd53be4c311cd9d6c09a13d7ca5478
hero-BA <suad.hero@gmail.com>
2021-03-22 03:55:37 +0100

    Modified exercise: Store multiple values in one variable

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -6,10 +6,12 @@ function myMonitorsFunction(arr) {
     var newMonitorsList = [...arr];
     // Only change code below this line
     var monitorsList = [];
-    
-
+    for (var i = 0; i < 3; i++) {
+        monitorsList.push(monitorsListArray[i], i + 1);
+    }
+    return monitorsList;
     // Only change code above this line
 }
 
-console.log(myMonitorsFunction());  // Change this line
+console.log(myMonitorsFunction(monitorsListArray));  // Change this line
 module.exports = myMonitorsFunction;
