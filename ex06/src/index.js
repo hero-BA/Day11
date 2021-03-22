340d7c6cabfe3d0a809ad18b46d71ee81a02e400
hero-BA <suad.hero@gmail.com>
2021-03-22 10:39:37 +0100

    Added exercise: Store multiple values in one variable

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,11 +1,16 @@
 // Only change code below this line
 function multiplyArrayFunction(myArray) {
-  var myArray = [...myArray];
-  let sum = 0;
-  let product = 1;
-  
-  
-  return [sum, product];
+  var sum = 0;
+  var product = 1;
+
+  for (var i = 0; i < myArray.length; i++) {
+    for (var j = 0; j < myArray[i].length; j++) {
+      sum += myArray[i][j];
+      product *= myArray[i][j];
+      myArray.push([sum, product]);
+    }
+  }
+  return myArray;
 }
 // Only change code above this line
 console.log(multiplyArrayFunction([2], [5, 6, 7], [8, 9])); // Change this line
